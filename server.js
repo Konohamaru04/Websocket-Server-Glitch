8b1bb5f32518767502015d0ace581f6d4cbbde3d
Glitch (glitch-hello-node) <none>
2023-06-01 21:57:15 +0000

    🏔🙅 Checkpoint ./server.js:58612493/1801

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
 // Create a new WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
@@ -7,12 +7,16 @@ const wss = new WebSocket.Server({ port: 8080 });
 const clients = new Set();
 
 // Handle new WebSocket connections
-wss.on("connection", (ws) => {
+wss.on('connection', (ws) => {
   // Add the new client to the set
   clients.add(ws);
 
+  // Log client information
+  console.log(`New client connected: ${ws._socket.remoteAddress}:${ws._socket.remotePort}`);
+
   // Handle incoming messages from the client
-  ws.on("message", (data) => {
+  ws.on('message', (data) => {
+    console.log(data)
     // Broadcast the drawing data to all connected clients
     clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
@@ -22,8 +26,11 @@ wss.on("connection", (ws) => {
   });
 
   // Handle WebSocket connection close
-  //ws.on("close", () => {
+  ws.on('close', () => {
     // Remove the client from the set
-    //clients.delete(ws);
-  //});
+    clients.delete(ws);
+
+    // Log client disconnection
+    console.log(`Client disconnected: ${ws._socket.remoteAddress}:${ws._socket.remotePort}`);
+  });
 });
